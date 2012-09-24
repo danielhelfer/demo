a78b668492bd546eed67df730517441de20fc912
Daniel Helfer <daniel.helfer@gmx.ch>
2012-09-24 17:03:28 +0200

    Initial commit

---
+++ config/deploy.rb
@@ -0,0 +1,29 @@
+set :application, "Demo"
+set :repository,  "set your repository location here"
+
+set :scm, :git
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+set :deploy_to, "/webapps/demo"
+
+default_run_options[:pty] = true
+
+role :web, "192.168.1.33"                          # Your HTTP server, Apache/etc
+role :app, "192.168.1.33"                          # This may be the same as your `Web` server
+role :db,  "192.168.1.33", :primary => true # This is where Rails migrations will run
+#role :db,  "your slave db-server here"
+
+# if you want to clean up old releases on each deploy uncomment this:
+# after "deploy:restart", "deploy:cleanup"
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+# namespace :deploy do
+#   task :start do ; end
+#   task :stop do ; end
+#   task :restart, :roles => :app, :except => { :no_release => true } do
+#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+#   end
+# end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demo::Application.config.secret_token = '28141427308a595f32c8e12e59bbbdcc815dce2d47b6d29529ca1e101cbfaf121c7e9762af480870383ec4b3c6a0544b023ca72a98071948a25f5edb3c2edce8'
